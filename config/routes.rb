84d4837d445617d8a5a67e8fac36b7847e822ff5
Hannah Lehman <hjlehman@gmail.com>
2015-02-27 22:03:04 -0600

    Fixed merge conflicts.

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,8 @@ Rails.application.routes.draw do
 
   resources :todos
 
-<<<<<<< Updated upstream
-  root 'todos#new'
-=======
   root to: 'todos#index'
->>>>>>> Stashed changes
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
